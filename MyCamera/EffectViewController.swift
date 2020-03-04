710b9b5b2f8fe1e6a88cc220d11741a700e5ebe8
KaitoKoike <kaikai.poteti@keio.jp>
2020-03-04 17:48:23 +0900

    アプリアイコンの追加．エフェクトの複数種類の導入

--- MyCamera/EffectViewController.swift
+++ MyCamera/EffectViewController.swift
@@ -37,10 +37,74 @@ class EffectViewController: UIViewController {
 
     @IBOutlet weak var effectImage: UIImageView!
     
+    
+    // フィルタ名を列挙した配列を作成する
+    let filterArray = ["CIPhotoEffectMono",
+                       "CIPhotoEffectChrome",
+                       "CIPhotoEffectFade",
+                       "CIPhotoEffectInstant",
+                       "CIPhotoEffectNoir",
+                       "CIPhotoEffectProcess",
+                       "CIPhotoEffectTonal",
+                       "CIPhotoEffectTransfer",
+                       "CISepiaTone"
+    ]
+    
+    //どのフィルターを用いているのかを示すindex
+    var filter_idx = 0
+    
     @IBAction func effectButtonAction(_ sender: Any) {
+        if let image = originalImage {
+            //フィルター名を指定
+            let filterName = filterArray[filter_idx]
+            //filter_idxを次の数字に変更
+            filter_idx += 1
+            //配列の長さに収める
+            filter_idx = filter_idx % filterArray.count
+            
+            //元々の画像の回転角度を取得
+            let rotate = image.imageOrientation
+            //UIImage形式の画像をCIImage形式に変換
+            let inputImage = CIImage(image: image)
+            
+            // フィルターの種類を引数で指定された種類を指定してCIFilterのインスタンスを取得
+            guard let effectFilter = CIFilter(name: filterName) else {
+                return
+            }
+            
+            //effectのパラメータを初期化
+            effectFilter.setDefaults()
+            //インスタンスにエフェクトする元画像を設定
+            effectFilter.setValue(inputImage, forKey: kCIInputImageKey)
+            //エフェクト後のCIImage形式の画像を取り出す
+            guard let outputImage = effectFilter.outputImage else {
+                return
+            }
+            //CIContextのインスタンスを取得
+            let ciContext = CIContext(options: nil)
+            
+            // エフェクト後の画像をCIContext上に描画し，結果をcgImageとしてCGImage形式の画像を取得
+            guard let cgImage = ciContext.createCGImage(outputImage, from: outputImage.extent) else {
+                return
+            }
+            
+            // エフェクト後の画像をDGImage形式からUIImage形式に変更．その際に回転角度を指定，そしてImageViewに表示
+            effectImage.image = UIImage(cgImage: cgImage, scale: 1.0, orientation: rotate)
+            
+
+        }
     }
     
     @IBAction func shareButtonAction(_ sender: Any) {
+        if let shareImage = effectImage.image?.resize() {
+            let shareItems = [shareImage]
+            
+            let controller = UIActivityViewController(activityItems: shareItems, applicationActivities: nil)
+            
+            controller.popoverPresentationController?.sourceView = view
+            
+            present(controller,animated: true,completion: nil)
+        }
     }
     
     @IBAction func closeButtonAction(_ sender: Any) {
